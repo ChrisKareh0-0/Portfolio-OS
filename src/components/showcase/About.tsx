686ea0e03219f7b46e89c6435daf09803b4a763f
Chris <chriskareh@proton.me>
2024-04-23 08:05:16 -0700

    Deploy Production Vercel

---
+++ src/components/showcase/About.tsx
@@ -0,0 +1,180 @@
+import React from 'react';
+import me from '../../assets/pictures/workingAtComputer.jpg';
+import meNow from '../../assets/pictures/currentme.jpg';
+import { Link } from 'react-router-dom';
+import ResumeDownload from './ResumeDownload';
+
+export interface AboutProps {}
+
+const About: React.FC<AboutProps> = (props) => {
+    return (
+        // add on resize listener
+        <div className="site-page-content">
+            {/* <img src={me} style={styles.topImage} alt="" /> */}
+            <h1 style={{ marginLeft: -16 }}>Welcome</h1>
+            <h3>I'm Chris Kareh</h3>
+            <br />
+            <div className="text-block">
+                <p>
+                    I'm a software engineer currently working at BSA! In 2023 I graduated from Arab Open University
+                    with my BS in Computer Science.
+                </p>
+                <br />
+                <p>
+                    Thank you for taking the time to check out my portfolio. I
+                    really hope you enjoy exploring it as much as I enjoyed
+                    building it. If you have any questions or comments, feel
+                    free to contact me using{' '}
+                    email{' '}
+                    <a href="mailto:chriskareh@proton.me">
+                        chriskareh@proton.me
+                    </a>
+                </p>
+            </div>
+            <ResumeDownload />
+            <div className="text-block">
+                <h3>About Me</h3>
+                <br />
+                <p>
+                My love for technology and programming started at a young age when I received my first computer as a gift. 
+                From that moment on, I was captivated by the endless possibilities that technology offered. 
+                Spending countless hours tinkering with software and writing simple programs, 
+                I found a sense of fulfillment and excitement that I couldn't find anywhere else. 
+                As I delved deeper into the world of programming, my passion only grew stronger, 
+                driving me to constantly learn and explore new technologies. 
+                The ability to create something out of nothing with just a few lines of code felt like magic to me, 
+                fueling my enthusiasm and determination to pursue a career in the tech industry.
+                </p>
+                <br />
+                {/* <div className="captioned-image">
+                    <img src={me} style={styles.image} alt="" />
+                    <p>
+                        <sub>
+                            <b>Figure 1:</b> A real photo of me developing this
+                            website :)
+                        </sub>
+                    </p>
+                </div> */}
+
+                <p>
+                    I started programming more seriously in high school,
+                    initially with game development.
+                    I went on to do a ton of passion projects, This is where FireLink Studios came to life 
+                    {/* <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://www.linkedin.com/in/scott-bass-189a7919b/"
+                    >
+                        Scott Bass
+                    </a> */}
+                    . We worked on many projects together, Websites,
+                    game projects, apps, and more.
+                    {/* <Link to="/projects/software">Software Projects</Link> page. */}
+                </p>
+                <br />
+                <p>
+                In 2023, I applied for a React Native developer position at Sync, 
+                feeling somewhat lost with minimal JavaScript experience under my belt. 
+                When they requested an app as part of the assessment, I turned to a friend for help. 
+                Surprisingly, I was accepted, and from there, I rapidly absorbed knowledge, 
+                transitioning from a novice with no real-world experience to a capable individual who could tackle tasks, 
+                albeit a bit slowly, but just enough to avoid suspicion.
+                As I gained more experience, I flourished, until an overseas company offered me a much better position as a Business Systems Analyst (BSA). In the team I was part of, 
+                we accomplished what four entire companies before us had failed to achieve. However, this success came at a steep price — I found myself working 13 to 14 hours daily, 
+                pushing my limits and sacrificing my health for the sake of success.
+                Now, I find myself seeking a position where I can thrive comfortably and maintain a healthier work-life balance.
+                </p>
+                <br />
+                <br />
+                <div style={{}}>
+                    <div
+                        style={{
+                            flex: 1,
+                            textAlign: 'justify',
+                            alignSelf: 'center',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <h3>My Hobbies</h3>
+                        <br />
+                        <p>
+                            Beyond software, I have a lot of hobbies that I
+                            enjoy doing in my free time. The more tangible
+                            hobbies I have is{' '}
+                            {/* <Link to="/projects/music">Music Production</Link>{' '} */}
+                             creating{' '}
+                            <Link to="/projects/art">Digital Art</Link>. You can
+                            read more about each of these on their respective
+                            pages under my projects tab. Some other hobbies I
+                            enjoy are working out, cooking, and (unsurprisingly)
+                            playing video games.
+                        </p>
+                        <br />
+                        {/* <p>
+                            In college, I was an active member in the fraternity
+                            Sigma Alpha Epsilon and held multiple positions in
+                            the chapter. I met a lot of amazing people through
+                            my fraternity and thoroughly enjoyed the community.
+                        </p> */}
+                    </div>
+                    {/* <div style={styles.verticalImage}>
+                        <img src={meNow} style={styles.image} alt="" />
+                        <p>
+                            <sub>
+                                <b>Figure 2:</b> Me, April 2022
+                            </sub>
+                        </p>
+                    </div> */}
+                </div>
+                <br />
+                <br />
+                <p>
+                    Thanks for reading about me! I hope that you enjoy exploring
+                    the rest of my portfolio website and everything it has to
+                    offer. 
+                </p>
+                <br />
+                <p>
+                    If you have any questions or comments I would love to hear
+                    them. You can shoot me an email
+                    at{' '}
+                    <a href="mailto:chriskareh@proton.me">
+                    chriskareh@proton.me
+                    </a>
+                    <br/>
+                    <br/>
+                    Even though the UI of this website is Windows Styled, I only daily drive linux based distros XD
+                </p>
+            </div>
+        </div>
+    );
+};
+
+const styles: StyleSheetCSS = {
+    contentHeader: {
+        marginBottom: 16,
+        fontSize: 48,
+    },
+    image: {
+        height: 'auto',
+        width: '100%',
+    },
+    topImage: {
+        height: 'auto',
+        width: '100%',
+        marginBottom: 32,
+    },
+    verticalImage: {
+        alignSelf: 'center',
+        // width: '80%',
+        marginLeft: 32,
+        flex: 0.8,
+
+        alignItems: 'center',
+        // marginBottom: 32,
+        textAlign: 'center',
+        flexDirection: 'column',
+    },
+};
+
+export default About;
---
+++ src/components/showcase/Experience.tsx
@@ -0,0 +1,281 @@
+import React from 'react';
+import ResumeDownload from './ResumeDownload';
+
+export interface ExperienceProps {}
+
+const Experience: React.FC<ExperienceProps> = (props) => {
+    return (
+        <div className="site-page-content">
+            <ResumeDownload />
+            <div style={styles.headerContainer}>
+                <div style={styles.header}>
+                    <div style={styles.headerRow}>
+                        <h1>BSA</h1>
+                        <a
+                            rel="noreferrer"
+                            target="_blank"
+                            href={'https://www.bsa.qa'}
+                        >
+                            <h4>www.bsa.qa</h4>
+                        </a>
+                    </div>
+                    <div style={styles.headerRow}>
+                        <h3>Frontend Engineer</h3>
+                        {/* <b>
+                            <p>Summer 2020 - Fall 2021</p>
+                        </b> */}
+                    </div>
+                </div>
+            </div>
+            <div className="text-block">
+                <p>
+                    Targeted towards large businesses, that wants to show their work to the world through digital solutions
+                </p>
+                <br />
+                <ul>
+                    <li>
+                        <p>
+                        I excel in creating responsive designs that provide optimal user experiences across various devices and screen sizes.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Designed and implemented multiple features to
+                            increase app usability and user experience while
+                            ensuring the quality, maintainability and
+                            scalability of the front end as the user base grew
+                            by over 50,000.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Coordinated major refactors targeted towards app
+                            optimization and performance resulting in a smoother
+                            user experience and accomplished by eliminating
+                            redundant re-renders and API calls by over 50%.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Directed and executed an internal migration of 3
+                            individual repositories to a single monorepo,
+                            greatly reducing overhead for developing new
+                            features, fixing bugs, and managing dependencies.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Rebuilt the website with React and shared mobile app
+                            components allowing users to access a wide variety
+                            of app interactions entirely on the web, which will result of
+                            over 700,000 total site visits.
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <div style={styles.headerContainer}>
+                <div style={styles.header}>
+                    <div style={styles.headerRow}>
+                        <h1>WeCanSync</h1>
+                        <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href={'https://wecansync.com'}
+                        >
+                            <h4>www.wecansync.com</h4>
+                        </a>
+                    </div>
+                    <div style={styles.headerRow}>
+                        <h3>Frontend Engineer</h3>
+                        {/* <b>
+                            <p>Fall 2021 - Spring 2022</p>
+                        </b> */}
+                    </div>
+                </div>
+            </div>
+            <div className="text-block">
+                <p>
+                During my tenure at the company as a front-end developer, one aspect I truly appreciated was the constant pressure to meet project deadlines. 
+                This pressure pushed me to enhance my problem-solving abilities, often leading me to work overtime in order to deliver tasks on time.
+                </p>
+                <br />
+                <ul>
+                    <li>
+                        <p>
+                         Proficient in Front-end Technologies, expertise in creating modern and intuitive user interfaces.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                         Version Control Systems,  Proficient in using Git for version control, facilitating collaboration with teams and ensuring efficient code management
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                        Performance Optimization: Skilled in optimizing front-end performance, employing strategies to minimize page load times, reduce HTTP requests, and optimize assets for improved performance
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                         Adept at troubleshooting and debugging code efficiently using developer tools and other debugging techniques
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <div style={styles.headerContainer}>
+                <div style={styles.header}>
+                    <div style={styles.headerRow}>
+                        <h1>FireLink Studios</h1>
+                        <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href={'https://kfxnyc.com/'}
+                        >
+                            {/* <h4>www.kfxnyc.com</h4> */}
+                        </a>
+                    </div>
+                    <div style={styles.headerRow}>
+                        <h3>My FreeLance Company-ish</h3>
+                        {/* <b>
+                            <p>Fall 2019, Summer 2021</p>
+                        </b> */}
+                    </div>
+                </div>
+            </div>
+            <div className="text-block">
+                <p>
+                We began our journey in video game development, launching two titles in beta. 
+                Despite our efforts, they didn't quite meet our expectations, prompting us to retire them. 
+                However, this shift has led us to focus on meeting the diverse demands of our clients in website and mobile development.
+                As we continue to hone our skills and expand our offerings, 
+                we're eagerly looking forward to the day when we can self-fund and bring our own creative visions to life.
+                </p>
+                <br />
+                {/* <h3 style={styles.indent}>Projects I've worked on:</h3>
+                <ul>
+                    <li style={styles.row}>
+                        <p></p>
+                        <p>
+                            [{' '}
+                            <a
+                                href="https://www.sho.com/ray-donovan"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                SHOWTIME
+                            </a>{' '}
+                            ]
+                        </p>
+                    </li>
+                    <li style={styles.row}>
+                        <p>• Ray Donovan: The Movie</p>
+                        <p>
+                            [{' '}
+                            <a
+                                href="https://www.sho.com/titles/3508117/ray-donovan-the-movie"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                SHOWTIME
+                            </a>{' '}
+                            ]
+                        </p>
+                    </li>
+                    <li style={styles.row}>
+                        <p>• Hightown</p>
+                        <p>
+                            [{' '}
+                            <a
+                                href="https://www.starz.com/us/en/series/hightown/57463"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                STARZ
+                            </a>{' '}
+                            ]
+                        </p>
+                    </li>
+                    <li style={styles.row}>
+                        <p>• Bull</p>
+                        <p>
+                            [{' '}
+                            <a
+                                href="https://www.cbs.com/shows/bull/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                CBS
+                            </a>{' '}
+                            ]
+                        </p>
+                    </li>
+                    <li style={styles.row}>
+                        <p>• At Home with Amy Sedaris</p>
+                        <p>
+                            [{' '}
+                            <a
+                                href="https://www.trutv.com/shows/at-home-with-amy-sedaris"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                truTV
+                            </a>{' '}
+                            ]
+                        </p>
+                    </li>
+                </ul> */}
+            </div>
+        </div>
+    );
+};
+
+const styles: StyleSheetCSS = {
+    header: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width: '100%',
+    },
+    skillRow: {
+        flex: 1,
+        justifyContent: 'space-between',
+    },
+    skillName: {
+        minWidth: 56,
+    },
+    skill: {
+        flex: 1,
+        padding: 8,
+        alignItems: 'center',
+    },
+    progressBar: {
+        flex: 1,
+        background: 'red',
+        marginLeft: 8,
+        height: 8,
+    },
+    hoverLogo: {
+        height: 32,
+        marginBottom: 16,
+    },
+    headerContainer: {
+        alignItems: 'flex-end',
+        width: '100%',
+        justifyContent: 'center',
+    },
+    hoverText: {
+        marginBottom: 8,
+    },
+    indent: {
+        marginLeft: 24,
+    },
+    headerRow: {
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+    },
+    row: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+};
+
+export default Experience;
